044f0dbeb0654f337d624e45490b34dc3add1b0b
Marc Gimeno <gimenocervantesmarc@gmail.com>
2023-11-13 08:00:31 +0100

    V2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let Client = require('ssh2-sftp-client');
 let sftp = new Client();
 let fs = require('fs');
+const { table } = require('table'); 
 require('dotenv').config();
 
 
@@ -76,13 +77,13 @@ function writeLog(text) {
 }
 
 // Funció per descarregar els fitxers del servidor
-async function downloadFiles(remotePath, localPath, backupPath) {
+async function downloadFiles(remotePath, localPath) {
     let files = await sftp.list(remotePath);
+    let downloadedFiles = [];  
     for (let i = 0; i < files.length; i++) {
         let file = files[i];
         if (file.type == '-') {
             await sftp.fastGet(`${remotePath}/${file.name}`, `${localPath}/${file.name}`);
-            await sftp.fastGet(`${remotePath}/${file.name}`, `${backupPath}/${file.name}`);
             // si el fitxer que hem descarregat a local no te extensió .txt li afegim
             let localFile = checkExtension(file.name);
             fs.rename(`${localPath}/${file.name}`, `${localPath}/${localFile}`, (err) => {
@@ -90,9 +91,11 @@ async function downloadFiles(remotePath, localPath, backupPath) {
                     writeLog(err);
                 }
             });
+            downloadedFiles.push(localFile);  
             writeLog(`* ${file.name} descarregat i copiat correctament. *`);
         }
     }
+    return downloadedFiles;
 }
 
 // Funció per eliminar els fitxers del servidor
@@ -107,7 +110,6 @@ async function deleteFiles(remotePath) {
     }
 }
 
-
 // Funció principal 
 async function main() {
     createLogs();
@@ -115,25 +117,39 @@ async function main() {
     createLocalFolder(LOCAL_GPA);
     createLocalFolder(LOCAL_ZBE);
     createLocalFolder(LOCAL_CORREUS);
-    //createLocalFolder(BACKUP_TRM);
-    //createLocalFolder(BACKUP_GPA);
-    //createLocalFolder(BACKUP_ZBE);
-    //createLocalFolder(BACKUP_CORREUS);
     try {
         await sftp.connect(config);
         writeLog('Connexió amb el servidor SFTP establerta correctament.');
-        await downloadFiles(REMOTE_TRM, LOCAL_TRM, BACKUP_TRM);
-        await downloadFiles(REMOTE_GPA, LOCAL_GPA, BACKUP_GPA);
-        await downloadFiles(REMOTE_ZBE, LOCAL_ZBE, BACKUP_ZBE);
-        await downloadFiles(REMOTE_CORREUS, LOCAL_CORREUS, BACKUP_CORREUS);
+
+        // Descargar archivos y obtener la lista de nombres de archivos descargados
+        const downloadedFilesTRM = await downloadFiles(REMOTE_TRM, LOCAL_TRM);
+        const downloadedFilesGPA = await downloadFiles(REMOTE_GPA, LOCAL_GPA);
+        const downloadedFilesZBE = await downloadFiles(REMOTE_ZBE, LOCAL_ZBE);
+        const downloadedFilesCORREUS = await downloadFiles(REMOTE_CORREUS, LOCAL_CORREUS);
+
+        // Eliminar archivos en el servidor
         await deleteFiles(REMOTE_TRM);
         await deleteFiles(REMOTE_GPA);
         await deleteFiles(REMOTE_ZBE);
         await deleteFiles(REMOTE_CORREUS);
+
         writeLog('Connexió amb el servidor SFTP tancada correctament.');
+
+        // Mostrar nombres de archivos descargados en forma de tabla
+        const tableData = [
+            ['Títol', 'Fitxers descarregats'],
+            ['TRM', downloadedFilesTRM.length > 0 ? downloadedFilesTRM.join('\n') : '0'],
+            ['GPA', downloadedFilesGPA.length > 0 ? downloadedFilesGPA.join('\n') : '0'],
+            ['ZBE', downloadedFilesZBE.length > 0 ? downloadedFilesZBE.join('\n') : '0'],
+            ['Correus', downloadedFilesCORREUS.length > 0 ? downloadedFilesCORREUS.join('\n') : '0']
+        ];
+
+        console.log(table(tableData));
+
         setTimeout(() => {
             console.log("Proces executat correctament.");
-        }, "6000")
+        }, "6000");
+
         await sftp.end();
         process.exit(0);
     } catch (err) {
@@ -142,5 +158,5 @@ async function main() {
     }
 }
 
-// Executar main
+// Ejecutar main
 main();
