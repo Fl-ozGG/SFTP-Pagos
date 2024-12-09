670379b2d8f87a3da7055576767f0c7f08b393b8
IgnasiFloz <ignasi.gurrea@ext.ambinformacio.cat>
2024-12-09 12:58:44 +0100

    funciona en la carpeta ZBE LOCAL

---
+++ fixLastLane.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+// Ruta de la carpeta
+const LOCAL_ZBE = process.env.LOCAL_ZBE;
+// Lee el contenido de la carpeta
+fs.readdir(LOCAL_ZBE, (err, archivos) => {
+  if (err) {
+    console.error('Error al leer la carpeta:', err);
+    return;
+  }
+
+  // Filtra solo los archivos (si quieres excluir subdirectorios)
+  archivos.forEach(archivo => {
+    if (archivo.charAt(0) != 'K') {
+      const rutaCompleta = path.join(LOCAL_ZBE, archivo);
+      fs.stat(rutaCompleta, (err, stats) => {
+        if (err) {
+          console.error('Error al obtener información del archivo:', err);
+          return;
+        }
+        if (stats.isFile()) {
+          // Si es un archivo, lo leemos
+          fs.readFile(rutaCompleta, 'utf8', (err, data) => {
+
+            let lineas = data.split('\n');
+            let numFinal;
+            let lineaInsertar;
+
+            // Encuentra el número final y genera la nueva línea a insertar
+            lineas.forEach((linea, index) => {
+              if (linea == lineas[lineas.length - 3]) {
+                numFinal = linea.slice(49, 54);
+                lineaInsertar = lineas[lineas.length - 2].slice(0, 49) + numFinal + '                                              \r'
+                console.log(numFinal)
+                // aqui quiero añadir la logica necesaria para que escriba en mi archivo txt la variable lineaInsertar, reemplazando la linea anterior que hubiese en este indice
+                lineas[lineas.length - 2] = lineaInsertar
+
+                const texto = lineas.join('\n'); // Esto los separará por espacios, usa '\n' para nuevas líneas
+
+                // Escribir el texto a un archivo
+                fs.writeFile(rutaCompleta, texto, (err) => {
+                  if (err) {
+                    console.error('Error al escribir el archivo:', err);
+                  } else {
+                    console.log('Archivo guardado correctamente.');
+                  }
+                });
+
+
+              }
+            });
+            console.log(lineas)
+
+
+          });
+        }
+      });
+    }
+  });
+});
+
+
